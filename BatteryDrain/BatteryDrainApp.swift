2a12dcc53f2a90041add59b1dc5394a4eefcd5dc
Ryan Plitt <ryan.plitt@icloud.com>
2025-03-29 22:08:28 -0600

    Initial Commit

---
+++ BatteryDrain/BatteryDrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryDrainApp.swift
+//  BatteryDrain
+//
+//  Created by Ryan Plitt on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct BatteryDrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
